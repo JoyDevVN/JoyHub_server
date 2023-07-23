6ea93194586f306ef16beffe11cdbf80d49901e2
Khanh Nguyen <Kn11072003a@gmail.com>
2023-07-23 20:07:22 +0700

    Update api.js

--- src/api.js
+++ src/api.js
@@ -3,9 +3,7 @@ import { json, urlencoded } from 'body-parser';
 import cors from 'cors';
 import serverless from 'serverless-http';
 
-// const productsRouter = require('./routes/products.route');
-// import accRouter from './routes/account.route';
-// import roleRouter from './routes/role.route';
+// routes
 import authRouter from './routes/auth.route';
 const router = Router();
 
@@ -20,18 +18,8 @@ router.get('/', (req, res) => {
     res.send('Welcome to the JoyServe API');
 });
 
-// app.use('/.netlify/functions/api/products', productsRouter);
-// app.use('/.netlify/functions/api/accounts', accRouter);
-// app.use('/.netlify/functions/api/roles', roleRouter);
 app.use('/.netlify/functions/api/auth', authRouter);
 app.use('/.netlify/functions/api', router);
 
-// error handler middleware
-// app.use((err, req, res, next) => {
-//     const statusCode = err.statusCode || 500;
-//     console.error(err.message, err.stack);
-//     res.status(statusCode).json({ message: err.message });
-//     return;
-// });
 export default app;
 export const handler = serverless(app);
